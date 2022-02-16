d319f36119f7bf2b9dc7a2a67d0a481e79e0d818
Taeyang Jin <sun@jupiterflow.com>
2022-02-16 21:06:59 +0900

    2022년 2월 16일 ex01 파일 추가

---
+++ kotlin-playground/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.6.10"
+}
+
+group = "ac.weekly"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
