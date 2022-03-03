70b64574c8bc5e5a6437be52903e912f2f5a780e
원종필 <rocknzero1@gmail.com>
2022-03-04 01:15:36 +0900

    Update ex03.kt

--- koground/src/ex20220302/ex03.kt
+++ koground/src/ex20220302/ex03.kt
@@ -3,13 +3,14 @@ package ex20220302
 class LazyTest {
     lateinit var inputValue: String
     val x: Int by lazy {inputValue.length} //by 는 위임
-    //
+    // by lazy = 호출시점에서 최초 1회 초기
+    // by lazy 는 lateinit과 다르게 null 허용
 }
 
 fun main() {
     val lazyTest = LazyTest()
     lazyTest.inputValue = "abc"
     println(lazyTest.x)
-    lazyTest.inputValue = "abcdefgh"
+    lazyTest.inputValue = "abcdefgh" //재초기화가 안됨
     println(lazyTest.x)
 }
